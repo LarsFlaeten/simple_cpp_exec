0c7540854b91717379dfa8fce813c032870402f0
LarsFlaeten <lars.flaeten@gmail.com>
2019-11-22 22:52:27 +0100

    Add command linde options

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -10,12 +10,43 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+	cxxopts::Options options(argv[0], "Simple c++ project skeleton");
+    options.add_options()
+        ("h,help", "Print help")
+        ("first", "First number to add", cxxopts::value<int>())
+		("second", "Second number to add", cxxopts::value<int>())
+		("positional",
+       "Positional arguments: these are the arguments that are entered "
+       "without an option", cxxopts::value<std::vector<std::string>>());
+        ;
+	options.parse_positional({"first", "second", "positional"});
+ 	auto result = options.parse(argc, argv);
+	   
+    if(result.count("help"))
+    {
+        cout << options.help({""}) << endl;
+        return 0;
+    }
+
+	int first = 2; // default
+    if(result.count("first"))
+    {
+        first = result["first"].as<int>();
+    } 
+
+	int second = 3; // default
+    if(result.count("second"))
+    {
+        second = result["second"].as<int>();
+    } 
+
+
     example::Example ex;
 
 
 
     cout << "Hello World!" << endl;
     
-    cout << 2 << " + " << 3 << " = " << ex.add(2,3) << std::endl;
+    cout << first << " + " << second << " = " << ex.add(first,second) << std::endl;
     return 0;
 }
